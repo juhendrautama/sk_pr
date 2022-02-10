37ef7ad71b3b6ef121d3b7df0626eee7d681d5e3
juhendrautama <hendracungkryng@gmail.com>
2022-02-10 10:13:26 +0700

    upload slider baru ganti yang lama

--- application/views/tools/slider.php
+++ application/views/tools/slider.php
@@ -7,7 +7,7 @@
           <div class="carousel-inner">
 
             <div class="carousel-item active" data-bs-interval="10000">
-              <img  src="img/slider/0003.jpg" class="d-block w-100" alt="...">
+              <img  src="img/slider/0004.jpg" class="d-block w-100" alt="...">
               <div class="carousel-caption d-none d-md-block">
                
               </div>
